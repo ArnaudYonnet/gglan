f16dbf90cf09dac0deef76304fae02e7109155be
Thibaud <thibaud29@protonmail.com>
2018-01-24 19:50:06 +0100

    Table rank

---
+++ database/migrations/2018_01_24_122725_create_rank_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRankTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rank', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('id_jeu');
+            $table->string('nom');
+            $table->timestamps();
+        });
+
+        Schema::table('rank', function (Blueprint $table) {
+            $table->foreign('id_jeu')
+                  ->references('id')->on('jeu')
+                  ->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rank');
+    }
+}
