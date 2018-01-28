a8db81da16d54c0ff3fdef413ea73f0e16b0219b
Thibaud <thibaud29@protonmail.com>
2018-01-28 17:40:05 +0100

    Move files

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -15,14 +15,11 @@ class ProfilController extends Controller
         if ($profil->id == Auth::id()) 
         {
             // Si l'id est celui de la personne connectée
-            return view('profil')->with('profil', $profil);
+            return view('profil.profil')->with('profil', $profil);
         }
-        else 
-        {
-            // Si l'id n'est pas celui de la personne connectée
-            $pseudo = DB::table('users')->where('id', $id)->value('pseudo');
-            return redirect('joueurs/'. $pseudo);    
-        }
-        
+
+        // Si l'id n'est pas celui de la personne connectée
+        $pseudo = DB::table('users')->where('id', $id)->value('pseudo');
+        return redirect('joueurs/'. $pseudo);    
     }
 }
