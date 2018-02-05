1989571a9849bac5e6dd522fbf9abebe90dfd55a
Thibaud <thibaud29@protonmail.com>
2018-02-05 21:35:52 +0100

    Revision navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,81 @@
+{{--  <nav class="navbar navbar-dark bg-dark">
+    <ul class="list-unstyled list-inline">
+        <li class="list-inline-item"><a href="/">Accueil</a></li>
+        <li class="list-inline-item"><a href="/tournois">Anciens Tournois</a></li>
+        <li class="list-inline-item"><a href="/joueurs">Joueurs</a></li>
+        <li class="list-inline-item"><a href="/equipes">Equipes</a></li>
+    </ul>
+    @guest
+        <div class="btn-group">
+            <a class="btn btn-danger" href="{{ route('register') }}">Inscription</a>
+            <br />
+            <a class="btn btn-danger" href="{{ route('login') }}">Connexion</a>
+        </div>
+    @else
+        <div class="btn-group right">
+            <button type="button" class="btn btn-danger dropdown-toggle" data-hover="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->pseudo }}
+            </button>
+            <div class="dropdown-menu">
+                @if (Auth::user()->admin)
+                    <a class="dropdown-item" href="/admin">Admin</a>
+                @endif
+                <a class="dropdown-item" href="/profil/{{ Auth::id() }}">Mon Profil</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Déconnexion
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </div>
+        </div>
+    @endguest  
+</nav>  --}}
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+        <a class="navbar-brand" href="/">Accueil</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/tournois">Anciens Tournois</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/joueurs">Joueurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/equipes">Equipes</a>
+                </li>
+            </ul>
+        @guest
+            <div class="btn-group">
+                <a class="btn btn-danger" href="{{ route('register') }}">Inscription</a>
+                <br />
+                <a class="btn btn-danger" href="{{ route('login') }}">Connexion</a>
+            </div>
+        @else
+            <div class="btn-group right">
+                <button type="button" class="btn btn-danger dropdown-toggle" data-hover="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->pseudo }}
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="/profil/{{ Auth::id() }}">Mon Profil</a>
+                    <div class="dropdown-divider">
+                    </div>
+                    @if (Auth::user()->admin)
+                        <a class="dropdown-item" href="/admin">Admin</a>
+                    @endif
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Déconnexion
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            </div>
+        @endguest  
+    </div>
+</nav>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{--  <link rel="stylesheet" href="{{ asset('css/app.css') }}" >  --}}
+
+    <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
     <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }} "></script>
     <script src="{{ asset('js/app.js') }}"></script>
@@ -14,37 +16,13 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            Banniere
+            {{--  Banniere  --}}
         </div>
-        <div class="row">
-            <ul class="list-unstyled list-inline">
-                <li class="list-inline-item"><a href="/">Accueil</a></li>
-                <li class="list-inline-item"><a href="/tournois">Anciens Tournois</a></li>
-                <li class="list-inline-item"><a href="/joueurs">Joueurs</a></li>
-                <li class="list-inline-item"><a href="/equipes">Equipes</a></li>
-
-                @guest
-                    <li class="list-inline-item"><a href="{{ route('login') }}">Connexion</a></li>
-                @else
-                    <li><a href="/profil/{{ Auth::id() }}">Profil</a></li>
-                    @if (Auth::user()->admin)
-                        <li><a href="/admin">Admin</a></li>
-                    @endif
-                    <li>
-                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Déconnexion
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+        @include('layouts.navbar')
     </div>
 
     <div class="container-fluid">
-        <div class="container float-left col-lg-9">
+        <div class="container float-left col-lg-10">
             @yield('content')
         </div>
 
