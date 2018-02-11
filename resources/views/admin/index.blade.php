ae9e073cf9d4af003652560a9d4962f75a346a7b
Thibaud <thibaud29@protonmail.com>
2018-02-11 12:23:39 +0100

    Amelioration tableaux

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,33 +3,38 @@
 @section('content')
     <div class="row">
         <div class="col-lg-6">
-            <legend>Joueurs</legend>
-            <table class="table table-hover table-dark">
-                <thead>
-                    <th scope="col">ID</th>
-                    <th scope="col">Pseudo</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Edit</th>
-                </thead>
-                <tbody>
-                    @foreach ($joueurs as $joueur)
-                        <tr>
-                            <th scope="row"> {{ $joueur->id_public }} </th>
-                            <th scope="row">{{ $joueur->pseudo }}</th>
-                            <th scope="row"> {{ $joueur->email }} </th>
-                            <td>
-                                <a href="/admin/edit/joueur/{{$joueur->id}}"><i class="fa fa-edit"></i></a>
-                                &nbsp
-                                <a href="/admin/delete/joueur/{{$joueur->id}}"><i class="fa fa-trash"></i></a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Joueurs</h3>
+                </div>
+                <div class="box-body">
+                    <table id="joueurs" class="table table-bordered table-hover">
+                        <thead>
+                            <th>Id</th>
+                            <th>Pseudo</th>
+                            <th>E-mail</th>
+                            <th>Edit</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($joueurs as $joueur)
+                                <tr>
+                                    <td> {{ $joueur->id_public }} </td>
+                                    <td>{{ $joueur->pseudo }}</td>
+                                    <td> {{ $joueur->email }} </td>
+                                    <td>
+                                        <a href="/admin/edit/joueur/{{$joueur->id}}"><i class="fa fa-edit"></i></a> &nbsp
+                                        <a href="/admin/delete/joueur/{{$joueur->id}}"><i class="fa fa-trash"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
 
         <div class="col-lg-6">
-            <legend>Equipes</legend>
+{{--              <legend>Equipes</legend>
             <table class="table table-hover table-dark">
                 <thead>
                     <th scope="col">Nom</th>
@@ -38,17 +43,44 @@
                 </thead>
                 <tbody>
                     @foreach ($equipes as $key => $equipe)
-                    <tr>
-                        <th scope="row"> {{ $equipe->nom }} </th>
-                        <th scope="row">{{ $equipiers[$key]}} / 5</th>
-                        <td>
-                            <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a> &nbsp
-                            <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a>
-                        </td>
-                    </tr>
+                        <tr>
+                            <th scope="row"> {{ $equipe->nom }} </th>
+                            <th scope="row">{{ $equipiers[$key]}} / 5</th>
+                            <td>
+                                <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a> &nbsp
+                                <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
-            </table>
+            </table>  --}}
+
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Equipes</h3>
+                </div>
+                <div class="box-body">
+                    <table id="equipes" class="table table-bordered table-hover">
+                        <thead>
+                            <th>Nom</th>
+                            <th>Joueurs</th>
+                            <th>Edit</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($equipes as $key => $equipe)
+                                <tr>
+                                    <td> {{ $equipe->nom }} </td>
+                                    <td>{{ $equipiers[$key]}} / 5</td>
+                                    <td>
+                                        <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a> &nbsp
+                                        <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/layouts/admin/template.blade.php
+++ resources/views/layouts/admin/template.blade.php
@@ -69,9 +69,14 @@
         @include('layouts.admin.footer')
 
         <!-- jQuery 3 -->
-        <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-        <!-- Bootstrap 4 -->
-        <script src="{{ asset('js/bootstrap.min.js') }} "></script>
+        <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- DataTables -->
+        <script src="/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+        <!-- SlimScroll -->
+        <script src="/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
         <!-- FastClick -->
         <script src="/bower_components/fastclick/lib/fastclick.js"></script>
         <!-- AdminLTE App -->
@@ -81,13 +86,14 @@
         <!-- jvectormap  -->
         <script src="/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
         <script src="/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-        <!-- SlimScroll -->
-        <script src="/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
         <!-- ChartJS -->
         <script src="/bower_components/Chart.js/Chart.js"></script>
-        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="/dist/js/pages/dashboard2.js"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="/dist/js/demo.js"></script>
+
+        <script>
+            $(function () {
+                $('#joueurs').DataTable();
+                $('#equipes').DataTable();
+            })
+        </script>
     </body>
 </html>
