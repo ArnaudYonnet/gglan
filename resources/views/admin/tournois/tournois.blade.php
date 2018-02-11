2691d406a9de2e9a5667060b55d857e599a83c1b
Thibaud <thibaud29@protonmail.com>
2018-02-11 20:06:04 +0100

    Modification tournois

--- resources/views/admin/tournois/tournois.blade.php
+++ resources/views/admin/tournois/tournois.blade.php
@@ -4,7 +4,6 @@
 @include('sweetalert::view')
     <div class="row">
         <div class="col-lg-12">
-            
             <div class="box">
                 <div class="box-header">
                     <h3 class="box-title">Tournois</h3>
@@ -20,7 +19,8 @@
                             {{--  <th>Début</th>  --}}
                             {{--  <th>Fin</th>  --}}
                             <th>Jeu</th>
-                            <th>Edit</th>
+                            <th>Modifier</th>
+                            <th>Supprimer</th>
                         </thead>
                         <tbody>
                             @foreach ($tournois as $key => $tournoi)
@@ -30,8 +30,7 @@
                                         <td><span class="label label-success">Ouvert</span></td>
                                     @else
                                         <td><span class="label label-danger">Fermé</span></td>
-                                    @endif {{--
-                                    <td> {{ $tournoi->status }} </td> --}}
+                                    @endif
                                     <td> 
                                         {{ \Carbon\Carbon::parse($tournoi->date_deb)->format('d/m/Y') }} |
                                         {{ \Carbon\Carbon::parse($tournoi->date_deb)->diffInDays(\Carbon\Carbon::parse($tournoi->date_fin))+1 }} 
@@ -45,9 +44,11 @@
                                         {{ \Carbon\Carbon::parse($tournoi->date_fin)->format('d/m/Y') }}
                                     </td>  --}}
                                     {{--  <td> {{ \Carbon\Carbon::parse($tournoi->date_fin)->format('d/m/Y') }} </td>  --}}
-                                    <td> {{ $jeux[$key] }} </td>
+                                    <td> {{ $jeu_tournois[$key] }} </td>
                                     <td>
-                                        <a id="edit" href="/admin/edit/tournois/{{$tournoi->id}}"><i class="fa fa-edit"></i></a> &nbsp
+                                        <a id="edit" href="/admin/edit/tournois/{{$tournoi->id}}"><i class="fa fa-edit"></i></a>
+                                    </td>
+                                    <td>
                                         <a id="delete" href="/admin/delete/tournois/{{$tournoi->id}}"><i class="fa fa-trash"></i></a>
                                     </td>
                                 </tr>
