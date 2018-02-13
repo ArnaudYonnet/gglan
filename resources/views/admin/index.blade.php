17ade4ae2f66a38ce9d72958398d6ccf1cdd54b9
Thibaud <thibaud29@protonmail.com>
2018-02-13 14:13:09 +0100

    Supprime options de modifs

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,8 +13,8 @@
                             <th>Id publique</th>
                             <th>Pseudo</th>
                             <th>E-mail</th>
-                            <th>Modifier</th>
-                            <th>Supprimer</th>
+                            {{--  <th>Modifier</th>
+                            <th>Supprimer</th>  --}}
                         </thead>
                         <tbody>
                             @foreach ($joueurs as $joueur)
@@ -22,8 +22,8 @@
                                     <td> {{ $joueur->id_public }} </td>
                                     <td> {{ $joueur->pseudo }} </td>
                                     <td> {{ $joueur->email }} </td>
-                                    <td> <a href="/admin/edit/joueur/{{$joueur->id}}"><i class="fa fa-edit"></i></a> </td>
-                                    <td> <a href="/admin/delete/joueur/{{$joueur->id}}"><i class="fa fa-trash"></i></a> </td>
+                                    {{--  <td> <a href="/admin/edit/joueur/{{$joueur->id}}"><i class="fa fa-edit"></i></a> </td>
+                                    <td> <a href="/admin/delete/joueur/{{$joueur->id}}"><i class="fa fa-trash"></i></a> </td>  --}}
                                 </tr>
                             @endforeach
                         </tbody>
@@ -42,20 +42,16 @@
                         <thead>
                             <th>Nom</th>
                             <th>Joueurs</th>
-                            <th>Modifier</th>
-                            <th>Supprimer</th>
+                            {{--  <th>Modifier</th>  --}}
+                            {{--  <th>Supprimer</th>  --}}
                         </thead>
                         <tbody>
                             @foreach ($equipes as $key => $equipe)
                                 <tr>
                                     <td> {{ $equipe->nom }} </td>
                                     <td>{{ $equipiers[$key]}} / 5</td>
-                                    <td>
-                                        <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a>
-                                    </td>
-                                    <td>
-                                        <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a>
-                                    </td>
+                                    {{--  <td> <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a> </td>  --}}
+                                    {{--  <td> <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a> </td>  --}}
                                 </tr>
                             @endforeach
                         </tbody>
