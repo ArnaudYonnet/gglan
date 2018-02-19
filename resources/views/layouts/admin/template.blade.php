7e81dcc121afaa0f2abf4da9aa30f616c45e34f7
Thibaud <thibaud29@protonmail.com>
2018-02-19 19:06:09 +0100

    Ajoute favicon

--- resources/views/layouts/admin/template.blade.php
+++ resources/views/layouts/admin/template.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
         <title>GG-LAN | Dashboard</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
