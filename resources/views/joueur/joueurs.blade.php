60671a3328e25629c8cb7a4f3a6fb13c7535bfae
Thibaud <thibaud29@protonmail.com>
2018-03-04 10:05:11 +0100

    Affiche avatar liste joueurs

--- resources/views/joueur/joueurs.blade.php
+++ resources/views/joueur/joueurs.blade.php
@@ -30,7 +30,7 @@
                         @endif
                     @endforeach
                     <p class="card-text"> {{ $joueur->description }} </p>
-                    <img class="card-img-top" src="/img/profil.png" alt="Card image cap">
+                    <img class="card-img-top" src="{{ $joueur->avatar }}" alt="Card image cap">
                 </div>
             </div>
         @endforeach
