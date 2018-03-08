ba7eb9bda7efc45bf96a08e60ff69189e03e3df0
Thibaud <thibaud29@protonmail.com>
2018-03-08 18:03:53 +0100

    Ajout page reglement & titre site fixe

--- resources/views/layouts/templateHome.blade.php
+++ resources/views/layouts/templateHome.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title> @yield('title') </title>
+    <title>GG-LAN</title>
     
     <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
     <link rel="stylesheet" href="{{ asset('css/darkly.min.css') }}">
