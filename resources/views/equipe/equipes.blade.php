bf6604ca5cc2f2eff85c69e008f51b696403fea5
Thibaud <thibaud29@protonmail.com>
2018-03-08 15:03:38 +0100

    Card Columns

--- resources/views/equipe/equipes.blade.php
+++ resources/views/equipe/equipes.blade.php
@@ -9,25 +9,32 @@
 @section('content')
 {{--  @include('flash::message')  --}}
 @include('sweetalert::view')
+<div class="row">
     <br />
-    <h2>Place equipe restantes <b>{{16-count($equipes)}}</b></h2>
-    <div class="card-columns">
-        @foreach ($equipes as $equipe)
-            <div class="card" style="width: 20rem;">
-                <h5 class="card-header">
-                    <a href="/equipes/{{ $equipe->id }}/profil" class="text-danger"> {{ $equipe->nom_equipe }} </a>
-                </h5>
-                <div class="card-body">
-                    @foreach ($joueurs as $joueur)   
-                        @if ($equipe->id == $joueur->id_equipe)
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $joueur->pseudo }}</h6>   
-                        @endif                      
-                    @endforeach
-                    <p class="card-text"> {{ $equipe->description }} </p>
-                    <img class="card-img-top" src="/img/profil.png" alt="Card image cap">
+    <h3>Place equipe restantes: <b>{{16-count($equipes)}}</b></h3>
+
+    <div class="col-lg-11">
+        <div class="card-columns">
+            @foreach ($equipes as $equipe)
+                <div class="card w-80" >
+                    <h5 class="card-header">
+                        <a href="/equipes/{{ $equipe->id }}/profil" class="text-danger"> {{ $equipe->nom_equipe }} </a>
+                    </h5>
+                    <div class="card-body">
+                        <img class="card-img-top img-fluid" src="/img/profil.png" alt="{{ $equipe->nom_equipe }}">
+                        <p></p>
+                        @foreach ($joueurs as $joueur)   
+                            @if ($equipe->id == $joueur->id_equipe)
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $joueur->pseudo }}</h6>   
+                            @endif                      
+                        @endforeach
+                        <p class="card-text"> {{ $equipe->description }} </p>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
+</div>
+    
 
 @endsection
--- resources/views/joueur/joueurs.blade.php
+++ resources/views/joueur/joueurs.blade.php
@@ -6,34 +6,35 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12">
-            <h4 class="description">Joueurs inscrits: <b>{{count($joueurs)}}</b></h4>
-            <div class="row pt-md">
+        <br />
+        <h3>Joueurs inscrits: <b>{{count($joueurs)}}</b></h3>
+
+        <div class="col-lg-11">
+            <div class="card-columns">
                 @foreach ($joueurs as $joueur)
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <a href="/joueurs/{{ $joueur->pseudo }}">
-                            <div class="img-box">
-                                <img src="{{ $joueur->avatar }}" style="max-width: 250px" class="img-responsive" >
-                            </div>
-                        </a>
-                        <h1>
-                            <a href="/joueurs/{{ $joueur->pseudo }}" class="text-danger"> {{ $joueur->pseudo }} </a>
-                        </h1>
-                        @foreach ($ranks as $rank)
-                            @if ($rank->id_user == $joueur->id)
-                                <h2 class="card-subtitle mb-2 text-muted">
-                                    Rank: {{ $rank->nom }}
-                                </h2>
-                            @endif
-                        @endforeach
-                        @foreach ($equipes as $equipe)
-                            @if ($equipe->id_user == $joueur->id)
-                                <h2 class="card-subtitle mb-2 text-muted">
-                                    Equipe:  <a href="/equipes/{{ $equipe->id }}/profil" class="text-danger">{{ $equipe->nom_equipe }}</a>
-                                </h2> 
-                            @endif
-                        @endforeach
-                        <p>{{ $joueur->description }}</p>
+                    <div class="card w-80">
+                        <h5 class="card-header">
+                            <a href="/joueur/{{ $joueur->pseudo }}" class="text-danger"> {{ $joueur->pseudo }} </a>
+                        </h5>
+                        <div class="card-body">
+                            <img class="card-img-top img-fluid" src="{{ $joueur->avatar }}" alt="Avatar introuvable..." >
+                            <p></p>
+                            @foreach ($ranks as $rank)
+                                    @if ($rank->id_user == $joueur->id)
+                                        <h6 class="card-subtitle mb-2 text-muted">
+                                            Rank: {{ $rank->nom }}
+                                        </h6>
+                                    @endif
+                            @endforeach
+                            @foreach ($equipes as $equipe)
+                                    @if ($equipe->id_user == $joueur->id)
+                                        <h6 class="card-subtitle mb-2 text-muted">
+                                            Equipe:  <a href="/equipes/{{ $equipe->id }}/profil" class="text-danger">{{ $equipe->nom_equipe }}</a>
+                                        </h6> 
+                                    @endif
+                            @endforeach
+                            <p class="card-text"> {{ $joueur->description }} </p>
+                        </div>
                     </div>
                 @endforeach
             </div>
