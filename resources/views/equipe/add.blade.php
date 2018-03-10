0674b1e9f5974dfd7fbd78fbe4808cb7b3ab5085
Thibaud <thibaud29@protonmail.com>
2018-03-10 23:38:21 +0100

    Maxlength champ id

--- resources/views/equipe/add.blade.php
+++ resources/views/equipe/add.blade.php
@@ -4,7 +4,7 @@
         <div class="form-group{{ $errors->has('id_user') ? ' has-error' : '' }}">
             <label for="id_user" class="col-md-4 control-label">ID du joueur</label>
             <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                <input id="id_public" type="text" class="form-control" name="id_public" value="{{ old('id_public') }}" placeholder="Ex: hTp59" autofocus>
+                <input id="id_public" type="text" class="form-control" name="id_public" value="{{ old('id_public') }}" placeholder="Ex: hTp59" maxlength="5" autofocus>
                 @if ($errors->has('id_public'))
                     <span class="help-block">
                         <strong>{{ $errors->first('id_public') }}</strong>
