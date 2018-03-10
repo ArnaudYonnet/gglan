d03cbfbe1c8587f99d4b0e3fd2a5675e3a853a89
Thibaud <thibaud29@protonmail.com>
2018-03-10 11:55:28 +0100

    Change logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarTarget">
         <a class="navbar-brand" href="/">
-            <img src="/img/logov2.png" alt="GG-LAN" style='width: 130px;'>
+            <img src="/img/logov3.png" alt="GG-LAN" style='width: 130px;'>
         </a>
         
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
