353157f2723cc8104618a67e672b6b648c6b4ff9
Thibaud <thibaud29@protonmail.com>
2018-03-11 13:21:39 +0100

    Couleur compteur

--- resources/views/equipe/equipes.blade.php
+++ resources/views/equipe/equipes.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.template')
 @section('content')
 @include('sweetalert::view')
-    <h2>Place equipe restantes <b>{{16-count($equipes)}}</b></h2>
+    @if (16-count($equipes) <= 0)
+        <h2>Toutes les places sont prises !</h2>
+    @else
+        <h2 >Place equipe restantes: <b class="text-danger">{{16-count($equipes)}}</b></h2>
+    @endif
     <div class="row">
         @foreach ($equipes as $equipe)
             <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4">
