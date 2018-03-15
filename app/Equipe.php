2f3e2d30403a0bf265db563f528061e1bfeb5234
Thibaud <thibaud29@protonmail.com>
2018-03-15 15:51:07 +0100

    Fonction getCapitaine & getJoueurs
    Permet de récupérer les infos du capitaine de l'équipe et de ses joueurs (Le capitaine n'apparait pas dans la liste des joueurs de l'équipe)

--- app/Equipe.php
+++ app/Equipe.php
@@ -8,4 +8,40 @@ class Equipe extends Model
 {
     protected $table = "equipe";
     public $timestamps = false;
+
+
+    public function getJoueurs()
+    {
+        try {
+            $ids = \App\Appartenance::where('id_equipe', $this->id)
+                                    ->where('id_user', '!=', $this->getCapitaine()->id)
+                                    ->get();
+
+            $joueurs = array();
+            foreach ($ids as $id) 
+            {
+                $joueur = \App\User::find($id->id_user);
+                array_push($joueurs, $joueur);
+            }
+        }
+        catch (\Exception $e)
+        {
+            return false;
+        }
+        
+        return $joueurs;
+    }
+
+    public function getCapitaine()
+    {
+        try {
+            $capitaine = \App\User::where('id', $this->id_capitaine)->first();
+        }
+        catch (\Exception $e)
+        {
+            return false;
+        }
+        
+        return $capitaine;
+    }
 }
