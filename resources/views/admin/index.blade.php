756def1acf5aa60db9af5f9730f56175d318412f
Thibaud <thibaud29@protonmail.com>
2018-03-18 00:34:31 +0100

    Change lien

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,18 +14,14 @@
                             <th>Pseudo</th>
                             <th>E-mail</th>
                             <th>Type</th>
-                            {{--  <th>Modifier</th>
-                            <th>Supprimer</th>  --}}
                         </thead>
                         <tbody>
                             @foreach ($inscrits as $inscrit)
                                 <tr>
                                     <td> {{ $inscrit->id_public }} </td>
-                                    <td><a href="/joueurs/{{ $inscrit->pseudo }}" target="_blank">{{ $inscrit->pseudo }}</a></td>
+                                    <td><a href="/joueurs/{{ $inscrit->id }}" target="_blank">{{ $inscrit->pseudo }}</a></td>
                                     <td> {{ $inscrit->email }} </td>
                                     <td> {{ $inscrit->type }} </td>
-                                    {{--  <td> <a href="/admin/edit/joueur/{{$inscrit->id}}"><i class="fa fa-edit"></i></a> </td>
-                                    <td> <a href="/admin/delete/joueur/{{$inscrit->id}}"><i class="fa fa-trash"></i></a> </td>  --}}
                                 </tr>
                             @endforeach
                         </tbody>
@@ -45,21 +41,17 @@
                             <th>Nom</th>
                             <th>Joueurs</th>
                             <th>Inscrit</th>
-                            {{--  <th>Modifier</th>  --}}
-                            {{--  <th>Supprimer</th>  --}}
                         </thead>
                         <tbody>
                             @foreach ($equipes as $key => $equipe)
                                 <tr>
-                                    <td><a href="/equipes/{{ $equipe->id }}/profil" target="_blank">{{ $equipe->nom_equipe }}</a></td>
+                                    <td><a href="/equipes/{{ $equipe->id }}" target="_blank">{{ $equipe->nom_equipe }}</a></td>
                                     <td>{{ $equipiers[$key]}} / 5</td>
                                     @if ($inscritsTournois[$key])
                                         <td><span class="label label-success">Inscrit</span></td>
                                     @else
                                         <td><span class="label label-danger">Non inscrit</span></td>
                                     @endif
-                                    {{--  <td> <a href="/admin/edit/equipe/{{$equipe->id}}"><i class="fa fa-edit"></i></a> </td>  --}}
-                                    {{--  <td> <a href="/admin/delete/equipe/{{$equipe->id}}"><i class="fa fa-trash"></i></a> </td>  --}}
                                 </tr>
                             @endforeach
                         </tbody>
