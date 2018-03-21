3b310e8abd0287d932d5616ff19114d225b666b1
Thibaud <thibaud29@protonmail.com>
2018-03-21 11:10:04 +0100

    fix page size

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,8 @@
 <html>
     <head>
         <title>On revient bientôt !</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:100,600">
         <style>
