d2850486cb382b93eff82046bc7ce325b5264771
Thibaud <thibaud29@protonmail.com>
2018-03-21 12:49:38 +0100

    template mobile

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,10 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title>GG-LAN</title>
     
     <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
-    <link rel="stylesheet" href="{{ asset('css/darkly.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/darkly.min.css') }}" media="screen">
     <link rel="stylesheet" href="{{ asset('css/style.scss') }}">
     <link rel="stylesheet" href="{{ asset('css/fontawesome/fontawesome-all.min.css') }}">
 
