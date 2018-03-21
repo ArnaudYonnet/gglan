026c6f968267373903cf8533ed8f0fff5c744acc
Thibaud <thibaud29@protonmail.com>
2018-03-21 12:51:44 +0100

    template home mobile

--- resources/views/layouts/templateHome.blade.php
+++ resources/views/layouts/templateHome.blade.php
@@ -2,6 +2,8 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>GG-LAN</title>
     
     <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
