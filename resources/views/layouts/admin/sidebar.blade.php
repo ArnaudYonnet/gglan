6a6a1dff7539e9e248e3334ce4be472610506261
Thibaud <thibaud29@protonmail.com>
2018-03-23 22:53:54 +0100

    Rebuild sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,45 +1,14 @@
 <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-
-        <li class="header">GGLAN</li>
-        <li><a href="/admin/joueurs"><i class="fa fa-user"></i>Joueurs</a></li>
-        <li><a href="/admin/equipes"><i class="fa fa-users"></i>Equipes</a></li>
-        <li><a href="/admin/tournois"><i class="fa fa-trophy"></i>Tournois</a></li>
-        <li><a href="/admin/jeux"><i class="fa fa-gamepad"></i>Jeux</a></li>
-        <li><a href="/admin/ranks"><i class="fa fa-angle-double-up"></i>Ranks</a></li>
-
-        <li class="treeview">
-            <a href="#">
-                    <i class="fa fa-newspaper-o"></i> <span>Articles</span>
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                  </a>
-        
-            <ul class="treeview-menu">
-                <li><a href="/admin/articles"><i class="fa fa-list-ul"></i>Liste d'articles</a></li>
-                <li><a href="/admin/articles/create"><i class="fa fa-plus-circle"></i>Ecrire un article</a></li>
-            </ul>
-        </li>
-
-        <li class="treeview">
-            <a href="#">
-                    <i class="fa fa-globe"></i> <span>Partenaires</span>
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                  </a>
-        
-            <ul class="treeview-menu">
-                <li><a href="/admin/partenaires"><i class="fa fa-list-ul"></i>Liste des partenaires</a></li>
-                <li><a href="/admin/partenaires/create"><i class="fa fa-plus-circle"></i>Ajouter un partenaire</a></li>
-            </ul>
-        </li>
-
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">GGLAN</li>
+            <li><a href="/admin/joueurs"><i class="fa fa-user"></i>Joueurs</a></li>
+            <li><a href="/admin/equipes"><i class="fa fa-users"></i>Equipes</a></li>
+            <li><a href="/admin/tournois"><i class="fa fa-trophy"></i>Tournois</a></li>
+            <li><a href="/admin/jeux"><i class="fa fa-gamepad"></i>Jeux</a></li>
+            <li><a href="/admin/ranks"><i class="fa fa-angle-double-up"></i>Ranks</a></li>
+            <li><a href="/admin/articles"><i class="fa fa-newspaper-o"></i>Articles</a></li>
+            <li><a href="/admin/partenaires"><i class="fa fa-globe"></i>Partenaires</a></li>
         </ul>
     </section>
-  <!-- /.sidebar -->
 </aside>
