1a8d00dd477cdcdd1830c92670c01032cc2e80a1
Thibaud <thibaud29@protonmail.com>
2018-03-26 22:01:52 +0200

    update infos pratiques

--- resources/views/infos.blade.php
+++ resources/views/infos.blade.php
@@ -10,17 +10,10 @@
             <div class="col"> 
                 <h5>Rajouter des joueurs à mon équipe</h5>
                 <p class="text-justify">
-                    Pour rajouter des joueurs à son équipe il faut se munir de son id publique.<br />
-                    Il s'agit du code gris se trouvant à coté de son pseudo sur son profil <b>(Ne pas prendre en compte le '#')</b>. 
-                    <br />
-                    <img src="/img/info/info_id_publique.png" class="img-fluid">
-                    <br />
-                    Une fois en possession de l'id publique du joueur à rajouter à votre équipe,
-                    il suffit de le copier dans le champ prévue accessible sur votre page équipe.
-                    <br />
-                    <b>Seul le capitaine de l'équipe peut rajouter des joueurs à son équipe.</b>
-                    <br />
-                    <img src="/img/info/info_ajout_joueur.png" class="img-fluid">
+                    Pour ajouter un joueur dans votre équipe, il faut que vous soyez capitaine de l'équipe. <br />
+                    Vous trouverez ensuite sur le profil du joueur à ajouter un bouton d'ajout juste en dessous de son avatar,
+                    cliquez dessus et le joueur sera immédiatement rajouté à votre équipe 
+                    (Seulement si le joueur n'est pas déjà dans une équipe).
                 </p>
             </div>
 
@@ -30,11 +23,12 @@
                 <h5>S'inscrire pour la prochaine LAN</h5>
                 <p class="text-justify">
                     Pour pouvoir s'inscrire à la prochaine il faut <u>obligatoirement</u> que votre équipe soit complète (5 Joueurs).
-                    Une fois fait, vous verrez sur le profil de votre équipe un bouton comme ci-dessous: <br/> 
-                    <button class="btn btn-danger mb-4">S'inscrire pour la prochaine LAN</button>
+                    Une cela fois fait, vous n'avez qu'à vous rendre (Le capitaine de l'équipe) sur la page tournois et vous inscrire ! <br />
                     <br/>
-                    Si une LAN est programmé et est ouverte aux inscriptions vous pourrez cliquer dessus et ainsi enregistré votre équipe.<br/>
-                    Si vous avez inscrit votre équipe mais que vous voulez vous désister veuillez contacter le staff sur le discord <a href="https://discord.gg/mApqnDW" target="_blank">GG-LAN</a> (Le bureau, Membre de l'association ou encore Dev Web).
+                    
+                    <h4>
+                        Vous pouvez à tout moment contacter le staff sur le discord <a href="https://discord.gg/mApqnDW" target="_blank">GG-LAN</a> <br />
+                        (Le bureau, Membre de l'association ou encore Dev Web).</h4>
                 </p>
             </div>
 
