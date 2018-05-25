8050403ea7099fd48c9e8c93784579fd680808a9
Thibaud <thibaud29@protonmail.com>
2018-05-25 11:31:16 +0200

    adminSettings

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
+use App\Admin;
 
 class HomeController extends Controller
 {
@@ -25,5 +27,27 @@ class HomeController extends Controller
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function settings()
+    {
+        return view('admin.settings')->with('user', Auth::guard('admin')->user());
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = Admin::find($request->user_id);
+            $user->name = $request->name;
+            $user->password = bcrypt($request->password);
+        $user->save();
+
+        flash('Your account has been successfully updated !')->success();
+        return redirect()->back();
+
     }
 }
