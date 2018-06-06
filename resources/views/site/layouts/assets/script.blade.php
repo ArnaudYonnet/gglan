2697cdc063f7443dbee0499406180cfb6e159156
Thibaud <thibaud29@protonmail.com>
2018-06-06 21:43:32 +0200

    fix clear alert

--- resources/views/site/layouts/assets/script.blade.php
+++ resources/views/site/layouts/assets/script.blade.php
@@ -1,6 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-    crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js">
 </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" 
