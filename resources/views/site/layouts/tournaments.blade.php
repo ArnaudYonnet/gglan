6edad8cd698296b1c08322229ed00054a9544f85
Thibaud <thibaud29@protonmail.com>
2018-06-10 23:51:45 +0200

    add info

--- resources/views/site/layouts/tournaments.blade.php
+++ resources/views/site/layouts/tournaments.blade.php
@@ -26,8 +26,13 @@
                 </li>
 
                 <li class="list-item">
+                    - Cashprizes de 500€ sur CS:GO et 250€ sur LoL
+                </li>
+                
+                <li class="list-item">
                     - Inscription dans l'onglet <a href="/tournaments"> tournois</a>
                 </li>
+
 
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2647.935211159601!2d-4.463137984181008!3d48.41938813946978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816bbb2de6cdd13%3A0x59acde574801704a!2sBrest+open+campus!5e0!3m2!1sfr!2sfr!4v1528665553408"
                     width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
