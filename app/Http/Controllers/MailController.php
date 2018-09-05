711ceaa96a7f5ee0b4a9e35c9cc080f1e4ea4369
Thibaud <thibaud29@protonmail.com>
2018-09-06 01:09:02 +0200

    SendAcceptRequest Job

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,12 +6,11 @@ use \Carbon\Carbon;
 // Model
 use App\User;
 
-// Mail
-use App\Mail\AcceptRequest;
 use App\Mail\RefuseRequest;
 
 //Job
 use App\Jobs\SendJoinRequest;
+use App\Jobs\SendAcceptRequest;
 
 class MailController extends Controller
 {
@@ -22,9 +21,7 @@ class MailController extends Controller
 
     static public function accept($user_id)
     {
-        $user = User::find($user_id);
-
-        Mail::to($user)->send(new AcceptRequest());
+        SendAcceptRequest::dispatch($user_id)->delay(Carbon::now()->addSeconds(5));        
     }
 
     static public function refuse($user_id)
