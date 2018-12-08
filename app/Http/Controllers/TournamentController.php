7c230164b0bf6c92bc131cbc076db2d56ffb5ae5
Thibaud <thibaud29@protonmail.com>
2018-12-08 15:43:22 +0100

    register player to tournament place

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+use App\TournamentPlace;
 use App\Tournament;
+use App\User;
+use App\Team;
 
 class TournamentController extends Controller
 {
@@ -26,8 +30,22 @@ class TournamentController extends Controller
      */
     public function register(Request $request)
     {
+        // Add team to the tournament
         $tournament = Tournament::find($request->tournament_id);
         $tournament->teams()->attach($request->team_id);
+        
+        // Find the team
+        $team = Team::find($request->team_id);
+
+        // Add players of the team to the listing 
+        foreach ($team->players as $player) {
+            TournamentPlace::create([
+                'tournament_id' => $tournament->id,
+                'team_id'       => $team->id,
+                'user_id'       => $player->id,
+                'payed'         => false, 
+            ]);
+        }
 
         flash('You have been successfully registered for this tournament !')->success();
         return redirect()->back();
@@ -43,6 +61,10 @@ class TournamentController extends Controller
     {
         $tournament = Tournament::find($request->tournament_id);
         $tournament->teams()->detach($request->team_id);
+
+        // Find the team
+        $team = Team::find($request->team_id);
+        TournamentPlace::where('team_id', $request->team_id)->where('payed',0)->delete();
 
         flash('You have been successfully unregistered for this tournament !')->success();
         return redirect()->back();
