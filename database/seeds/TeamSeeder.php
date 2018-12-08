2f64fa2698479300e3616c3308b2416aebe983c3
Thibaud <thibaud29@protonmail.com>
2018-12-08 15:51:21 +0100

    update team seeder

--- database/seeds/TeamSeeder.php
+++ database/seeds/TeamSeeder.php
@@ -23,5 +23,29 @@ class TeamSeeder extends Seeder
 
         //Attach the captain to the team players relation
         $team->players()->attach(1);
+
+        // Attach other players to the team
+        $team->players()->attach(2);
+        $team->players()->attach(3);
+        $team->players()->attach(4);
+        $team->players()->attach(5);
+
+        //Create another team
+        $team = Team::create([
+            'name' => 'TestEnCarton',
+            'user_id' => 6,
+            'game_id' => 1,
+            'description' => 'Test',
+            'avatar' => null,
+        ]);
+
+        //Attach the captain to the team players relation
+        $team->players()->attach(6);
+
+        // Attach other players to the team
+        $team->players()->attach(7);
+        $team->players()->attach(8);
+        $team->players()->attach(9);
+        $team->players()->attach(10);
     }
 }
