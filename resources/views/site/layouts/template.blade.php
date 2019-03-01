08dcaaadae0a44ba262c0daf255295b2eb29ca52
Thibaud <thibaud29@protonmail.com>
2019-03-01 18:24:32 +0100

    add g_analyctics

--- resources/views/site/layouts/template.blade.php
+++ resources/views/site/layouts/template.blade.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @if (config('app.env') == "production")
+        <!-- Global Site Tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-91334247-1 "></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ' UA-91334247-1 ');
+        </script>
+    @endif
     <link rel="icon" type="image/png" href=" {{ asset('/img/favicon.png') }} ">
     @include('site.layouts.assets.css')
     <title> {{ config('app.name') }} </title>
